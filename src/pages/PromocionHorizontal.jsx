872416064e1b9c6a7da8115d47cbf6de6eca56f6
aidabereniceh-a11y <aidabereniceh@gmail.com>
2026-05-20 15:26:58 -0600

    Update PromocionHorizontal.jsx

--- src/pages/PromocionHorizontal.jsx
+++ src/pages/PromocionHorizontal.jsx
@@ -1,1 +1,11 @@
+export default function PromocionHorizontal() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Promoción Horizontal USICAMM</h1>
 
+      <p>
+        Recursos, estrategias y simuladores para promoción horizontal.
+      </p>
+    </div>
+  );
+}
