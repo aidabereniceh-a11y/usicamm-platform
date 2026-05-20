73e12e332bcf45056a4c30b29e58523d91d86c77
aidabereniceh-a11y <aidabereniceh@gmail.com>
2026-05-20 15:27:52 -0600

    Add GuiaUsicamm2026 component with content

--- src/pages/GuiaUsicamm2026.jsx
+++ src/pages/GuiaUsicamm2026.jsx
@@ -1,1 +1,11 @@
+export default function GuiaUsicamm2026() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Guía USICAMM 2026</h1>
 
+      <p>
+        Todo lo que necesitas para prepararte para USICAMM 2026.
+      </p>
+    </div>
+  );
+}
