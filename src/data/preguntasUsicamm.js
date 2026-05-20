127bf79862b8df7d4370b3a44c80c4bacee5df92
aidabereniceh-a11y <aidabereniceh@gmail.com>
2026-05-20 15:52:33 -0600

    Update preguntasUsicamm.js

--- src/data/preguntasUsicamm.js
+++ src/data/preguntasUsicamm.js
@@ -1,1 +1,17 @@
+export const preguntasUsicamm = [
+  {
+    categoria: "NEM",
 
+    pregunta: "¿Qué significa USICAMM?",
+
+    opciones: [
+      "Unidad del Sistema para la Carrera de las Maestras y los Maestros",
+      "Universidad del Sistema Educativo",
+      "Unidad Superior de Educación",
+      "Sistema Nacional Docente",
+    ],
+
+    correcta:
+      "Unidad del Sistema para la Carrera de las Maestras y los Maestros",
+  },
+];
