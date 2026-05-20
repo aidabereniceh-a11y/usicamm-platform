934c1a510d5b32ceb7f315987e3d4a4c733b1a89
aidabereniceh-a11y <aidabereniceh@gmail.com>
2026-05-20 15:28:10 -0600

    Add PlaneacionesNem component with educational content

--- src/pages/PlaneacionesNem.jsx
+++ src/pages/PlaneacionesNem.jsx
@@ -1,1 +1,11 @@
+export default function PlaneacionesNem() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Planeaciones NEM</h1>
 
+      <p>
+        Planeaciones educativas alineadas a la Nueva Escuela Mexicana.
+      </p>
+    </div>
+  );
+}
